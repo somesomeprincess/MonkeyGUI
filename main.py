a3471e270ed1a7fe68e0ed611ca5cd7369c8a026
Hippo Wu <ZMWU@royole.com>
2019-03-12 17:45:52 +0800

    v0.1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import subprocess, os, threading, yaml, shutil
+import subprocess, os, threading, yaml, shutil,zipfile
 from tkinter import ttk
 from time import *
 
@@ -14,7 +14,7 @@ class MonkeyGUI:
             shutil.copy('param_default.yml', 'param.yml')
             self.get_param()
         self.check_logfile_exist('monkeylog')
-        self.log_dir = os.path.abspath('monkeylog')+'\\'
+        self.log_dir = os.path.abspath('monkeylog')
 
     def check_logfile_exist(self,dir):
         if(not os.path.exists('/'.join([os.getcwd(),dir]))):
@@ -124,7 +124,10 @@ class MonkeyGUI:
         cb_comp.set('保存至电脑')
 
         # 运行
-        btn_run = tk.Button(self.frame, text='RUN', command=self.click_run)
+        self.run_strvar=tk.StringVar()
+        self.run_strvar.set("RUN")
+        btn_run = tk.Button(self.frame, textvariable=self.run_strvar, command=self.click_run_addstop)
+        #btn_run = tk.Button(self.frame, textvariable=self.run_strvar, command=self.click_run_changetext)
         btn_run.grid(row=rowrun, column=col_lable, padx=PADDING, pady=PADDING)
         btn_stop = tk.Button(self.frame, text='保存参数', command=self.closing_save_para)
         btn_stop.grid(row=rowrun, column=col_text, padx=PADDING, pady=PADDING)
@@ -152,14 +155,10 @@ class MonkeyGUI:
         combo_v.set('日志级别')
         combo_v['values'] = ['-v', '-v -v', '-v -v -v', '']
         combo_v['state'] = 'readonly'
-        # shebeihao.pack()
-        # btn_getshebeihao.pack()
-        # self.et_shebeihao.pack()
-        # self.root.protocol("WM_DELETE_WINDOW", self.closing_save_para)
-        # messagebox.askokcancel("退出吗")
         self.set_default_value()
         self.root.mainloop()
 
+    #设置默认参数
     def set_default_value(self):
         self.tx_runtimes.insert(0, self.param['runtimes'])
         self.tx_shebeihao.insert(0, self.param['deviceid'])
@@ -167,8 +166,13 @@ class MonkeyGUI:
         self.intvar_crash.set(self.param['crashgoon'])
         self.intvar_anr.set(self.param['anrgoon'])
         self.intvar_secur.set(self.param['securgoon'])
+        self.intvar_savelogcat.set(self.param['savelogcat'])
+        self.intvar_seperr.set(self.param['seperateerr'])
         self.tx_blackwrite.insert(0, self.param['bwlisfile'])
         self.tx_pkg.insert(0, self.param['pkg'])
+        self.tx_throttle.insert(0,self.param['throttle'])
+        self.tx_self.insert(0,self.param['self'])
+        self.tx_script.insert(0, self.param['monkeyscript'])
 
     def closing_save_para(self):
         with open('param.yml', 'w') as f:
@@ -177,8 +181,8 @@ class MonkeyGUI:
 
     def check_param(self):
         pkg = self.tx_pkg.get()
+        self.param['pkg'] = pkg
         if (pkg):
-            self.param['pkg'] = pkg
             if(',' in pkg):
                 pkg = ' '.join(['-p ' + i for i in pkg.split(',')])
             else :
@@ -191,14 +195,14 @@ class MonkeyGUI:
         self.param['runtimes'] = times
         shell = 'adb shell'
         shebeihao = str(self.tx_shebeihao.get())
+        self.param['deviceid'] = shebeihao
         if (shebeihao):
-            self.param['deviceid'] = shebeihao
             if (shebeihao == '-d'):
                 shell = 'adb -d shell'
             elif (shebeihao == '-e'):
                 shell = 'adb -e shell'
             else:
-                shell = ' '.join(['adb','-s','shell', shebeihao])
+                shell = ' '.join(['adb','-s', shebeihao,'shell'])
         crash_goon = self.intvar_crash.get()
         self.param['crashgoon'] = crash_goon
         crash = ''
@@ -229,8 +233,8 @@ class MonkeyGUI:
         else:
             throttle = '--randomize-throttle'
         seed = self.tx_seed.get()
+        self.param['seed'] = seed
         if (seed):
-            self.param['seed'] = seed
             seed = '-s ' + seed
         # 自定义
         selflize = self.tx_self.get()
@@ -243,14 +247,14 @@ class MonkeyGUI:
         self.param['logv'] = logv
         #monkeyscript
         script=self.tx_script.get()
+        self.param['monkeyscript'] = script
         if(script):
-            self.param['monkeyscript'] = script
             script = '-f '+script
             pkg = ''
         # 黑白名单
         file = self.tx_blackwrite.get()
+        self.param['bwlisfile'] = file
         if (file):
-            self.param['bwlisfile'] = file
             var = '--pkg-whitelist-file'
             if (self.blackwrite.get() == '黑名单'):
                 var = '--pkg-blacklist-file'
@@ -259,56 +263,122 @@ class MonkeyGUI:
             pkg = ''
         # 保存位置
         # 异常流
-        savewhat = self.strvar_comp.get()
-        if (savewhat == '保存至电脑'):
-            savepath = self.log_dir
-        else:
-            savepath = '/sdcard/'
 
-        sepererr = self.intvar_seperr.get()
-        logname = 'amonkey.txt'
-        errlog = 'amonkey_err.txt'
-        if (sepererr):
-            newsavepath = '1>%s 2>%s' % (savepath + logname, savepath + errlog)
-        else:
-            newsavepath = '>' + savepath + logname
-        # if (savewhat == '保存至手机'):
-        #     newsavepath = newsavepath + ' &'
+        issavalog=self.intvar_savelogcat.get()
+        self.param['savelogcat']=issavalog
+        cmd = ' '.join([shell, 'monkey', logv, seed, pkg, file, script, throttle, secur, crash, anr, selflize,
+                        times])
+        # 是否保存logcat
+        if(issavalog):
+            savewhat = self.strvar_comp.get()
+            if (savewhat == '保存至电脑'):
+                savepath = self.log_dir
+            else:
+                savepath = '/sdcard/'
 
-        self.text_log.insert(tk.END, '开始运行monkey..')
-        cmd = ' '.join([shell,'monkey', logv, seed, pkg, file, script, throttle, secur, crash, anr, selflize,
-                        times, newsavepath])
-        #去除空格
-        saveflag = (savewhat == '保存至手机')
-        if(saveflag):
-            cmd = cmd + '"'
-            cmd_lst = cmd.split(' ')
-            for i in range(len(cmd_lst) - 1, -1, -1):
-                if (cmd_lst[i] == 'monkey'):
-                    cmd_lst[i] = '"monkey'
-                elif (not cmd_lst[i]):
-                    cmd_lst.pop(i)
-        else:
-            cmd_lst = cmd.split(' ')
-            for i in range(len(cmd_lst) - 1, -1, -1):
-                if (not cmd_lst[i]):
-                    cmd_lst.pop(i)
-        cmd = ' '.join(cmd_lst)
+            sepererr = self.intvar_seperr.get()
+            self.param['seperateerr'] = sepererr
+            self.logname = 'monkey.txt'
+            errlog = 'monkey_err.txt'
+            if (sepererr):
+                newsavepath = ' 1>%s 2>%s' % (os.path.join(savepath,self.logname), os.path.join(savepath,errlog))
+            else:
+                newsavepath = ' >' + savepath + self.logname
+            # if (savewhat == '保存至手机'):
+            #     newsavepath = newsavepath + ' &'
+
+            self.text_log.insert(tk.END, '开始运行monkey..')
+            # cmd = ' '.join([shell,'monkey', logv, seed, pkg, file, script, throttle, secur, crash, anr, selflize,
+            #                 times, newsavepath])
+            cmd =cmd +newsavepath
+            #去除空格
+            saveflag = (savewhat == '保存至手机')
+            if(saveflag):
+                cmd = cmd + '"'
+                cmd_lst = cmd.split(' ')
+                for i in range(len(cmd_lst) - 1, -1, -1):
+                    if (cmd_lst[i] == 'monkey'):
+                        cmd_lst[i] = '"monkey'
+                    elif (not cmd_lst[i]):
+                        cmd_lst.pop(i)
+            else:
+                cmd_lst = cmd.split(' ')
+                for i in range(len(cmd_lst) - 1, -1, -1):
+                    if (not cmd_lst[i]):
+                        cmd_lst.pop(i)
+            cmd = ' '.join(cmd_lst)
         return cmd
 
     def click_run(self):
+        self.run_strvar.set("STOP")
         self.root.update_idletasks()
-        if(not self.check_has_devices()):
+        if (not self.check_has_devices()):
             self.text_log.insert(tk.END, 'NO Devices!!  Please Connect 1 device.')
             return
         savelogcat = self.intvar_savelogcat.get()
         cmd = self.check_param()
         if (cmd):
             self.text_log.insert(tk.END, cmd)
-            T2=threading.Thread(target=self.execute_monkey,args=(cmd,savelogcat,self.monkeycallback))
+            T2 = threading.Thread(target=self.execute_monkey, args=(cmd, savelogcat, self.monkeycallback))
             T2.setDaemon(True)
             T2.start()
             self.root.update_idletasks()
+
+    def click_run_addstop(self):
+        if(self.run_strvar.get()=="RUN"):
+            self.run_strvar.set("STOP")
+            self.root.update_idletasks()
+            if(not self.check_has_devices()):
+                self.text_log.insert(tk.END, 'NO Devices!!  Please Connect 1 device.')
+                return
+            savelogcat = self.intvar_savelogcat.get()
+            cmd = self.check_param()
+            if (cmd):
+                self.text_log.insert(tk.END, cmd)
+                T2=threading.Thread(target=self.execute_monkey,args=(cmd,savelogcat,self.monkeycallback))
+                T2.setDaemon(True)
+                T2.start()
+                self.root.update_idletasks()
+        else:
+            self.run_strvar.set("RUN")
+            self.stop_monkey()
+
+    def stop_monkey(self):
+        pidlst=self.getPid()
+        if(pidlst ==-1):
+            pass
+        else:
+            for index in range(len(pidlst)):
+                try:
+                    cmd = "adb shell kill "+pidlst[index]
+                    print('kill cmd  ='+cmd)
+                    subprocess.Popen(cmd,shell=True)
+
+                except Exception as e:
+                    print(e)
+    def getPid(self):
+        cmd = 'adb shell "ps -A|grep monkey"'
+        out = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        infos = out.stdout.read().splitlines()
+        print(infos)
+        pidlst=[]
+        if(len(infos)>=1):
+            for info in infos:
+                if(info):
+                    pid = info.split()[1].decode()
+                    print(pid)
+                    if(pid not in pidlst):
+                        pidlst.append(pid)
+            return pidlst
+        else:
+            return -1
+
+
+    def click_run_changetext(self):
+        if(self.run_strvar.get()=="RUN"):
+            self.run_strvar.set("STOP")
+        else:
+            self.run_strvar.set("RUN")
 
     def reconnect(self):
         cmd = 'adb reconnect'
@@ -318,7 +388,7 @@ class MonkeyGUI:
 
     def run_cmd(self, cmd):
         result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return result.stderr.read()
+        return result.stderr.read(encoding='UTF-8')
 
     def check_has_devices(self):
         NO_DEVICE_COUNT_LINE = 2
@@ -338,8 +408,23 @@ class MonkeyGUI:
         output = subprocess.Popen('adb shell monkey')
         self.et_shebeihao.insert(tk.END, output.stdout.readlines())
 
+    def ziplog(self):
+        zippath = self.log_dir + '.zip'
+        zipFile = zipfile.ZipFile(zippath, "w", zipfile.ZIP_DEFLATED)
+        for dirpath, dirnames, filenames in os.walk(self.log_dir):
+            print(dirpath, filenames)
+            fpath = dirpath.replace(self.log_dir, '')
+            print(fpath)
+            for filename in filenames:
+                zipFile.write(os.path.join(dirpath, filename), os.path.join(fpath, filename))
+        zipFile.close()
+
     def monkeycallback(self):
         self.text_log.insert(tk.END, '结束monkey..')
+
+        if(self.strvar_comp.get()=='保存至电脑'):
+            self.ziplog()
+        self.run_strvar.set("RUN")
         if(self.intvar_savelogcat.get()):
             self.p.terminate()
 
@@ -348,9 +433,10 @@ class MonkeyGUI:
 
         if (savelogcat):
             curtime = strftime('%F%H%M%S', localtime(time()))
-            txt = ''.join([self.log_dir,'log', curtime, '.txt'])
+            self.logname=''.join([curtime,'txt'])
+            txt = ''.join([self.log_dir,'\\','log', self.logname])
 
-            logcatcmd = 'adb logcat'
+            logcatcmd = 'adb logcat -b main'
             logtxt = open(txt, 'w+')
             self.p = subprocess.Popen(logcatcmd, stdout=logtxt, stderr=subprocess.PIPE)
         result.stdout.readline()
@@ -366,7 +452,7 @@ class MonkeyGUI:
             self.text_log.insert(tk.END, self.log_dir+' have NO file to analyse!!')
             return
         self.text_log.insert(tk.END, '正在分析logcat...请稍等。')
-        with open(file,encoding='utf-8') as f:
+        with open(file,encoding='UTF-8') as f:
             s=set()
             for line in f.readlines():
                 for key in abnormal_key:
